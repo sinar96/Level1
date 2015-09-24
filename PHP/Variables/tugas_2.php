c66006fbdb19ceef946443860097fef2b7aaae4a
sinar96 <sinarmustikaruslan@gmail.com>
2015-09-25 00:58:18 +0800

    Tugas 2

---
+++ PHP/Variables/tugas_2.php
@@ -0,0 +1,30 @@
+<?php
+//strings
+$a = (string) "Nama";
+$b = "Alamat";
+$c = (string) "Tempat lahir";
+$d = "Tanggal lahir";
+$e = (string) "Asal daerah";
+
+//number(integer)
+$d = 768;
+$e = (int) 875;
+$f = 9302;
+$g = (int) 83939;
+$h = 828728;
+
+//number(float)
+$i = (float) 6.4;
+$j = 7.3;
+$k = (float) 832.10;
+$l = 6373.2919;
+$m = (float) 672.5267;
+
+//boolean
+$jujur  = True;
+$bohong = False;
+$malas  = False;
+$salah  = False;
+$benar  = True;
+
+?>
---
+++ Python/Variables/tugas_2.py
@@ -0,0 +1,29 @@
+#strings
+a = "sinar"
+b = "kelas"
+c = "nim"
+d = "jurusan"
+e = "prodi"
+
+#numbers(integer)
+aa = 20
+bb = 30
+cc = 40
+dd = 400
+ee = 560
+
+#numbers(float)
+aaa = 2.5
+bbb = 7.34
+ccc = 8.34
+ddd = 6.323
+eee = 10.7673
+
+#Boolean
+female      = True
+male        = False
+electrical  = True
+akuntansi   = False
+_2b         = True
+ 
+ 
